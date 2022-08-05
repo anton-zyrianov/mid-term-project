ebbd88d80e0767cab55da1543995167b86109562
Antón Zyrianov <73389835+zantonz@users.noreply.github.com>
2022-08-05 14:10:52 +0200

    last changes

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,13 @@
+const buttonBurger = document.getElementById('button-menu');
+const navBurger = document.getElementById('nav-burger');
+const lockScroll = document.body;
+
+buttonBurger.addEventListener('click', () => {
+   buttonBurger.classList.toggle('active');
+   navBurger.classList.toggle('active');
+   lockScroll.classList.toggle('lock');
+});
+
 function addPost(event) {
    event.preventDefault(); 
 
@@ -40,9 +50,6 @@ function addPost(event) {
 
       return response.json();
 
-   })
-   .then((data) => {
-      console.log(data);
    });
 
 }
