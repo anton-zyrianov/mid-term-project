ca1fcbcc06578f5f1edfe27e5d5f1abf50bf9e7b
Antón Zyrianov <73389835+zantonz@users.noreply.github.com>
2022-08-05 14:11:33 +0200

    last changes

--- js/project.js
+++ js/project.js
@@ -1,4 +1,19 @@
+const buttonBurger = document.getElementById('button-menu');
+const navBurger = document.getElementById('nav-burger');
+const lockScroll = document.body;
 
+buttonBurger.addEventListener('click', () => {
+   buttonBurger.classList.toggle('active');
+   navBurger.classList.toggle('active');
+   lockScroll.classList.toggle('lock');
+});
+
+const buttonQuestions = document.getElementById('submit');
+const iconQuestions = document.querySelector('.questions-icono');
+
+buttonQuestions.addEventListener('click', () => {
+   iconQuestions.classList.toggle('clicked');
+});
 
 function getJsonIndividual() {
    fetch('https://jsonplaceholder.typicode.com/posts/1')
@@ -32,6 +47,43 @@ function getJsonProjects(){
          })
       });
 }
+
+function sendEmail(event) {
+   event.preventDefault(); 
+
+   let mail = document.getElementById("e-mail").value;
+
+   fetch("https://jsonplaceholder.typicode.com/posts", {     
+   
+   method: "POST",
+   
+   body: JSON.stringify({
+      mail:mail,
+   }),
+   
+   headers: {
+      "Content-type": "application/json; charset=UTF-8"
+   }
+   })
+   .then((response) => {
+
+      if(response.status === 201){
+         const elementSuccess = document.querySelector('.questions-success');
+         elementSuccess.style.display = "block";
+
+      } else {
+         elementFailed = document.querySelector('.question-failed');
+         elementFailed.style.display = "block";
+
+      }
+
+      return response.json();
+
+   });
+
+}
+
+document.getElementById("form-index").addEventListener("submit", sendEmail);
 
 getJsonProjects();
 
